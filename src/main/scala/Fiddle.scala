12a154d46518e1b00cfa12111d88eaa7705f3e41
Andrew Rothstein <andrew.rothstein@gmail.com>
2013-11-10 22:27:14 -0500

    first connection from elastic4s. I've got a lot to learn about Futures and Promises

---
+++ src/main/scala/Fiddle.scala
@@ -0,0 +1,21 @@
+import com.sksamuel.elastic4s.ElasticClient
+import com.sksamuel.elastic4s.ElasticDsl._
+
+import org.elasticsearch.common.settings.ImmutableSettings
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+import ExecutionContext.Implicits.global
+
+object Fiddle extends App {
+	val settings = ImmutableSettings.settingsBuilder()
+			.put("cluster.name", "elasticsearch_drew")
+			.build()
+	val client = ElasticClient.remote(settings, ("127.0.0.1", 9300))
+	val r = client.execute {
+	  search in "logstash-2013.11.10" query "first"
+	}
+	Await.ready(r, 10 seconds)
+	println(r.value)
+}
